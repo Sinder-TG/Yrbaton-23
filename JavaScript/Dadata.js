4af2393ea634ee7bf0bcf339597947db8ca02e72
Тигран <148631589+Sinder-TG@users.noreply.github.com>
2023-11-25 08:30:42 +0300

    Full css  main window

--- JavaScript/Dadata.js
+++
@@ -1,7 +0,0 @@
-$("#address").suggestions({
-    token: "3ee64e7edeea1353eed5038655d1808953f86ee7",
-    type: "ADDRESS",
-    onSelect: function (suggestion) {
-        console.log(suggestion);
-    },
-});
